15823f69929ce46d01b2e94b145889199ffad5c2
Jason Savell <jsavell@library.tamu.edu>
2021-04-28 15:05:59 -0500

    initial gifm implementation

---
+++ themes/TAMU/js/gifm.js
@@ -0,0 +1,51 @@
+//check with Catalog Service
+$(document).ready(function() {
+  var gifmBase = "https://api-dev.library.tamu.edu/get-it/";
+	var catalogName = 'folio';
+
+	var bibId = $("#hiddenInstanceId").val();
+	if (bibId) {
+		if ($(".getit").length > 0) {
+			$(".getit").each(function(index,button) {
+				$(this).append("<div class=\"buttons\"><div class=\"gifm-loading\"></div><i class=\"updates\"></i></div>");
+			});
+			var messageIncrement = 0;
+			var startTimer = function() {
+        patience = setInterval(function() {
+                      var messages = ["This title has a lot of volumes, so this may take a while.",
+                                      "We're still working on creating the Get It For Me buttons.",
+                                      "Please stay on the page. We're still working on it",
+                                      "Please stay on the page while we retrieve the information."];
+                      if (messageIncrement >= messages.length) {
+                        messageIncrement = 0;
+                      }
+                      $(".getit div.buttons .updates").text(messages[messageIncrement]);
+                      messageIncrement++;
+                    }, 15000);
+      };
+
+			$.ajax({
+				url: gifmBase+"catalog-access/get-html-buttons",
+				data: {"bibId": bibId,"catalogName":catalogName},
+				beforeSend: function() {
+					startTimer();
+				}
+			}).done(function(data) {
+				if (data.meta.status === "ERROR") {
+					$(".getit div.buttons i").text(data.meta.message);
+				} else {
+					$(".getit .buttons").html("");
+					$.each(data.payload.HashMap,function(mfhd,buttons) {
+						$.each(buttons,function(index,button) {
+							$("#holdingId_"+mfhd+" .buttons").append(button);
+						});
+					});
+				}
+			}).fail(function(xhr,status,error) {
+				$(".getit div.buttons").html("<i>Error retrieving holding details</i>");
+			}).always(function() {
+				clearInterval(patience);
+			});
+		}
+	}
+});
