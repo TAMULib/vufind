6921fd214d34a3b1452e2e23f2971da3cc762cfd
Jason Savell <jsavell@tamu.edu>
2024-07-15 09:42:33 -0500

    tamu js formatting to make vufind build workflow happy

--- themes/TAMU/js/tamu_fulltext.js
+++ themes/TAMU/js/tamu_fulltext.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
   let $sfxButton = $("#sfxButton");
   let title = $sfxButton.data("title");
   let issn = $sfxButton.data("issn");
   if (title && issn) {
     $.ajax({
-      url: gifmBase+"catalog-access/check-full-text",
-      data: {"title": title,"issn": issn}
-    }).done(function(data) {
+      url: gifmBase + "catalog-access/check-full-text",
+      data: { "title": title, "issn": issn }
+    }).done(function (data) {
       if (data.payload.Boolean == true) {
         $("#sfxRow td").children().removeClass("hidden");
       }
