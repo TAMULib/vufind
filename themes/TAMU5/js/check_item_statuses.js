ed945d3d1537fb5bf04f47e1821cf8a018730eae
Jason Savell <jsavell@tamu.edu>
2025-10-15 13:03:57 -0500

    only attempt to inject buttons if element is on the dom

--- themes/TAMU5/js/check_item_statuses.js
+++ themes/TAMU5/js/check_item_statuses.js
@@ -59,8 +59,11 @@ VuFind.register('itemStatuses', function ItemStatuses() {
             for (const [holdingId, buttonsData] of Object.entries(data.payload.HashMap)) {
               if (buttonsData) {
                 buttonsData.buttons.forEach(button => {
-                  let buttonHtml = '<a target="_blank" class="'+button.cssClasses+'" href="https://'+button.linkHref+'">'+button.linkText+'</a>';
-                  el.querySelector("#getit_"+holdingId+"_"+button.itemKey).innerHTML = buttonHtml;
+                  const getItEl = el.querySelector("#getit_h"+holdingId+"_i"+button.itemKey);
+                  if (getItEl) {
+                    let buttonHtml = '<a target="_blank" class="'+button.cssClasses+'" href="https://'+button.linkHref+'">'+button.linkText+'</a>';
+                    getItEl.innerHTML = buttonHtml;
+                  }
                 });
               }
             }
