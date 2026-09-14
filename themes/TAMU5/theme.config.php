2b9fc6773912f7d777b334fff98afedab1feb15a
RickyLeeII <144821434+RickyLeeII@users.noreply.github.com>
2026-09-14 09:56:33 -0500

    Add TAMU5 theme from tamu-vufind-10.2.1

---
+++ themes/TAMU5/theme.config.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'extends' => 'bootstrap5',
+    'favicon' => 'favicon.ico',
+    'css' => [
+        'home-page.css'
+    ],
+    'helpers' => [
+        'factories' => [
+            'TAMU\View\Helper\Root\Record' => 'VuFind\View\Helper\Root\RecordFactory',
+            'VuFind\View\Helper\Root\RecordDataFormatter' => 'TAMU\View\Helper\Root\RecordDataFormatterFactory',
+        ],
+        'aliases' => [
+            'record' => 'TAMU\View\Helper\Root\Record'
+        ]
+    ]
+];
